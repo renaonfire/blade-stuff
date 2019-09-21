0368c7072d8541fa6880171d6b25ad0e5ef5afc1
Rena Brook <renabrook@Renas-MacBook-Pro.local>
2019-09-21 13:36:07 +0100

    changed js flicker

--- public/JS/services.js
+++ public/JS/services.js
@@ -1,3 +1,60 @@
+//image movement
+var $body = document.querySelector('body')
+
+var AutoScroll = function(scrollList) {
+  this._scrollList = scrollList;
+  this._startTop = $body.scrollTop
+  this._isInAnimate = false
+  var that = this
+  $(window).on('mousewheel',function(e) {
+    
+    that.scroll(e)
+    return false
+  })
+}
+AutoScroll.prototype.getEndTop = function(currentTop,e) {
+  var scrollTopList = []
+  Array.prototype.forEach.call(this._scrollList, function(d) {
+    scrollTopList.push(d.offsetTop)
+  })
+  scrollTopList.push(currentTop)
+  scrollTopList.sort(function(a, b) {
+    return a > b
+  })
+  if (e.originalEvent.wheelDelta<0)
+    return scrollTopList[scrollTopList.indexOf(currentTop) + 1]
+  
+  else
+    return scrollTopList[scrollTopList.indexOf(currentTop) - 1]
+}
+AutoScroll.prototype.scroll = function(e) {
+  var startTop
+  var endTop
+  var currentTop
+  var that = this
+  if (!this._isInAnimate) {
+    startTop = this._startTop
+    currentTop = $body.scrollTop
+    endTop = this.getEndTop(currentTop,e)
+    this._isInAnimate = true
+    var durTime = 1000
+    if (startTop === endTop) {
+      that._isInAnimate = false
+      return
+    }
+    $('body').animate({
+      scrollTop: endTop
+    }, durTime, function() {
+      that._startTop = $body.scrollTop
+      that._isInAnimate = false
+    })
+  }
+}
+
+var as = new AutoScroll(document.querySelectorAll('.content-box'));
+
+
+//content animation
 // Detect request animation frame
 var scroll = window.requestAnimationFrame ||
              // IE Fallback
@@ -20,9 +77,8 @@ function loop() {
 // Call the loop for the first time
 loop();
 
-// Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
-  // special bonus for those using jQuery
+  
   if (typeof jQuery === "function" && el instanceof jQuery) {
     el = el[0];
   }
@@ -38,88 +94,6 @@ function isElementInViewport(el) {
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
   );
 }
-
-
-
-
-
-
-//--- DEFINE a reusable animation function: ---//
-function scrollThere(targetElement, speed) {
-  // initiate an animation to a certain page element:
-  $('html, body').stop().animate(
-    { scrollTop: targetElement.offset().top }, // move window so target element is at top of window
-    speed, // speed in milliseconds
-    'swing' // easing
-  ); // end animate
-} // end scrollThere function definition
-
-//--- START SCROLL EVENTS ---//
-// detect a mousewheel event (note: relies on jquery mousewheel plugin):
-$(window).on('mousewheel', function (e) {
-
-  // get Y-axis value of each div:
-  var div1y = $('#first').offset().top,
-      div2y = $('#second').offset().top,
-      div3y = $('#third').offset().top,
-      div4y = $('#fourth').offset().top,
-      
-      // get window's current scroll position:
-      lastScrollTop = $(this).scrollTop(),
-      // for getting user's scroll direction:
-      scrollDirection,
-      // for determining the previous and next divs to scroll to, based on lastScrollTop:
-      targetUp,
-      targetDown,
-      // for determining which of targetUp or targetDown to scroll to, based on scrollDirection:
-      targetElement;
-
-  // get scroll direction:
-  if ( e.deltaY > 0 ) {
-    scrollDirection = 'up';
-  } else if ( e.deltaY <= 0 ) {
-    scrollDirection = 'down';
-  } // end if
-
-  // prevent default behavior (page scroll):
-  e.preventDefault();
-
-  // condition: determine the previous and next divs to scroll to, based on lastScrollTop:
-  if (lastScrollTop === div1y) {
-    targetUp = $('#first');
-    targetDown = $('#second');
-  } else if (lastScrollTop === div2y) {
-    targetUp = $('#first');
-    targetDown = $('#third');
-  } else if (lastScrollTop === div3y) {
-    targetUp = $('#second');
-    targetDown = $('#fourth');
-  } else if (lastScrollTop < div2y) {
-    targetUp = $('#first');
-    targetDown = $('#second');
-  } else if (lastScrollTop < div3y) {
-    targetUp = $('#second');
-    targetDown = $('#third');
-  } else if (lastScrollTop < div4y) {
-    targetUp = $('#third');
-    targetDown = $('#fourth');
-  } else if (lastScrollTop > div4y) {
-    targetUp = $('#fourth');
-    targetDown = $('#fourth');
-  } // end else if
-
-  // condition: determine which of targetUp or targetDown to scroll to, based on scrollDirection:
-  if (scrollDirection === 'down') {
-    targetElement = targetDown;
-  } else if (scrollDirection === 'up') {
-    targetElement = targetUp;
-  } // end else if
-
-  // scroll smoothly to the target element:
-  scrollThere(targetElement, 400);
-
-}); // end on mousewheel event
-//--- END SCROLL EVENTS ---//
 
 
 //hamburger
